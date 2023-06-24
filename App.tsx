8ba06f4e76e4067fb65eed01dee60907a38a6131
Sophia Freitas <sophia_rezende@hotmail.com>
2023-06-24 14:01:15 -0300

    primeira pagina

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import Quiz from './src/pages/Quiz'
+import Home from './src/pages/Home';
+import Teste from './src/pages/Teste';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+     <Home />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Components/CardMovies/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+      cardImage: {
+        width: '100%',
+        height: 500,
+        marginTop: 5
+      },
+      titleContainer: {
+        position: 'absolute',
+        top: 1,
+        backgroundColor: '#fff3',
+        fontWeight: 'bold',
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+      },
+      cardTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+
+})
