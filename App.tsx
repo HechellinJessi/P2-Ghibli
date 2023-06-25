c93c1f9e19474cbf5bee9a744f9005e303f1f2fb
Hechellin <hechellinjmv@gmail.com>
2023-06-25 15:52:38 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Quiz from './src/pages/Quiz'
 import Home from './src/pages/Home';
-import Teste from './src/pages/Teste';
+
 
 export default function App() {
   return (
--- src/pages/BemVindo/index.tsx
+++ src/pages/BemVindo/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import { View, Text, Image } from 'react-native';
 
-import Button from "../../Components/Button" 
+import Button from "../../Components/Buttons"
 
 import { styles } from './styles';
 
 import telaone from '../../assets/Image/imgOne.png'
 
-export default function Welcome(){
+export default function BmVindo(){
     return(
         <View style={styles.container}>
             <Image style={styles.image} source={telaone}/>
