ed667b673211ac69095a68a51a8485559d190452
Sophia Freitas <sophia_rezende@hotmail.com>
2023-06-27 17:43:47 -0300

    criação do registro

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import BemVindo from './src/pages/BemVindo'
-import Home from './src/pages/Home';
-import Diretores from './src/pages/Diretores'
-import Favorites from './src/pages/Favoritos';
 import { StatusBar } from 'expo-status-bar';
 import { BottomTabNavigator } from './src/routes/BottomTabNavigator'
 
@@ -11,8 +7,12 @@ import { BottomTabNavigator } from './src/routes/BottomTabNavigator'
 export default function App() {
   return (
     <>
-      <Home/>
+      <StatusBar
+        backgroundColor='#fff'
+        translucent={false}
+      />
+
+      <BottomTabNavigator/>
     </>
   );
 }
-
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+          container: {
+            marginTop: "50%",
+            margin: 15,
+
+          },
+
+          button:{
+            alignItems:"center",
+            backgroundColor: "#062451",
+            padding:12,
+            width: 300,
+            borderRadius:10,
+            marginTop: 100,
+            marginLeft:30,
+            color: 'white',
+            textAlign: "center"
+        },
+          
+      })
+      
