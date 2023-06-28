36bee5cffb4a56618634d2de64f736efa72d7c68
JuSMoraes <soaresjuliamoraes@gmail.com>
2023-06-28 17:16:54 -0300

    página do login estilizada

--- App.tsx
+++ App.tsx
@@ -4,18 +4,19 @@ import { StatusBar } from 'expo-status-bar';
 import { BottomTabNavigator } from './src/routes/BottomTabNavigator'
 import AboutUs from './src/pages/AboutUs';
 import Login from './src/pages/Login';
+import BemVindo from './src/pages/BemVindo';
 
 
 export default function App() {
   return (
     <>
-      <StatusBar
+      {/* <StatusBar
         backgroundColor='#fff'
         translucent={false}
       />
-      <BottomTabNavigator/>
+      <BottomTabNavigator/> */}
 
-       {/* <AboutUs/>  */}
+       <Login/> 
     </>
   );
 }
